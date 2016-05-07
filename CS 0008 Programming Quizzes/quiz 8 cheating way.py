6dee23229b821d4b4017e49f43350e98188cc3cf
Zekimar <zzffcrz@gmail.com>
2016-05-07 02:49:56 -0400

    Added files via upload

---
+++ CS 0008 Programming Quizzes/quiz 8 cheating way.py
@@ -0,0 +1,8 @@
+user = input ("enter a string: ")
+user = user.replace ("a", "")
+user = user.replace ("e", "")
+user = user.replace ("i", "")
+user = user.replace ("o", "")
+user = user.replace ("u", "")
+
+print (user)
