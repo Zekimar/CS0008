f4f42102b9403e637e570bb004a9a41cd630ce93
Kevin Good <zzffcrz@gmail.com>
2018-02-07 23:55:43 -0500

    Delete quiz 8 cheating way.py

--- CS 0008 Programming Quizzes/quiz 8 cheating way.py
+++
@@ -1,8 +0,0 @@
-user = input ("enter a string: ")
-user = user.replace ("a", "")
-user = user.replace ("e", "")
-user = user.replace ("i", "")
-user = user.replace ("o", "")
-user = user.replace ("u", "")
-
-print (user)
